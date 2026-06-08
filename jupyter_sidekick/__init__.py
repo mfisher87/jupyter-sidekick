2193b01ddac2252e6e7f83f580a73dde761a4d0e
Carl Boettiger <cboettig@berkeley.edu>
2026-06-08 10:50:31 -0700

    Release 0.3.0 (#23)

--- jupyter_sidekick/__init__.py
+++ jupyter_sidekick/__init__.py
@@ -1,6 +1,6 @@
 """jupyter-sidekick: Jupyter-native access to coding agents over the Agent Client Protocol."""
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
 def _jupyter_server_extension_points():
